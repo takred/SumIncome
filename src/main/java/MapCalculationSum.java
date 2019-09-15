665c99af7653dfc2127c27b6fd65246500604bf0
takred <sazonov.kos@yandex.ru>
2019-09-15 16:59:50 +0300

    Правки.

--- src/main/java/MapCalculationSum.java
+++ src/main/java/MapCalculationSum.java
@@ -1,28 +1,21 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MapCalculationSum {
-    private Map<String, Integer> usersData;
-    public MapCalculationSum(List<Income> usersData){
-        this.usersData = new HashMap<>();
-        for (int i = 0; i < usersData.size(); i++) {
-            this.usersData.put(usersData.get(i).getName(), 0);
+    private Map<String, Integer> totalIncomes;
+    public MapCalculationSum(List<Income> incomes){
+        this.totalIncomes = new LinkedHashMap<>();
+        for (int i = 0; i < incomes.size(); i++) {
+            Income income = incomes.get(i);
+            if (!this.totalIncomes.containsKey(incomes.get(i).getName())) {
+                this.totalIncomes.put(income.getName(), 0);
+            } else {
+                this.totalIncomes.put(income.getName(), this.totalIncomes.get(income.getName()) + income.getSum());
+            }
         }
-        for (int i = 0; i < usersData.size(); i++) {
-            Income income = usersData.get(i);
-            this.usersData.put(income.getName(), this.usersData.get(income.getName()) + income.getSum());
-        }
-    }
-    public Map<String, Integer> getUsersData(){
-        return usersData;
-    }
-    public void setUsersData(Map<String, Integer> usersData){
-        this.usersData = usersData;
     }
     public void printAllSum(){
-        for (Map.Entry<String, Integer> entry : usersData.entrySet()){
+        for (Iterator<Map.Entry<String, Integer>> iterator = totalIncomes.entrySet().iterator(); iterator.hasNext(); ) {
+            Map.Entry<String, Integer> entry = iterator.next();
             System.out.println(entry.getKey() + " - " + entry.getValue() + "р");
         }
     }
